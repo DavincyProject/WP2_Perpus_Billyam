07994c5d19a6a7814f8089bc19f100b9126610b6
Davincy Project <bdavincy.bd@gmail.comm>
2023-05-12 11:52:40 +0700

    fix admin page

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,10 +2,15 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Admin extends CI_Controller
 {
-    public function __construct()
+    function cek_login()
     {
-        parent::__construct();
-        cek_login();
+        $ci = get_instance();
+        if (!$ci->session->userdata('email')) {
+            $ci->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">Akses ditolak. Anda belum login!! </div>');
+            redirect('autentifikasi');
+        } else {
+            $role_id = $ci->session->userdata('role_id');
+        }
     }
     public function index()
     {
