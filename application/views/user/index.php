b1046e54f5cbb3beb3037cfbfd38859555cb79e8
Davincy Project <bdavincy.bd@gmail.com>
2023-05-25 15:01:32 +0700

    fix margin on profile container

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -6,11 +6,11 @@
         </div>
     </div>
     <div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
+        <div class="mx-3 row no-gutters">
             <div class="col-md-4">
                 <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img" alt="...">
             </div>
-            <div class="col-md-8">
+            <div class="col-md-8 ">
                 <div class="card-body">
                     <h5 class="card-title"><?= $user['nama']; ?></h5>
                     <p class="card-text"><?= $user['email']; ?></p>
